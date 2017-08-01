a490140153e8b7e8ab30aa6edc99b35ac71f93d4
JanakJayaweera <30425810+JanakJayaweera@users.noreply.github.com>
2017-08-01 18:34:03 +0530

    Sign In window added

---
+++ src/itpjfml/FXMLDocumentController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itpjfml;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+/**
+ *
+ * @author USER
+ */
+public class FXMLDocumentController implements Initializable {
+    
+  
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    /**
+     *
+     */
+    @FXML
+    public void userSelect(){
+        FXMLLoader fxmlLoader = new FXMLLoader (getClass().getResource("LoginPage.fxml"));
+        try {
+            Parent root = (Parent)fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setFullScreen(true);
+            stage.show();
+            ITPJFML.window.close();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+}
